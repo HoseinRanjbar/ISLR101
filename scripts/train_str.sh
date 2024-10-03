cb03d80e03a40c7a360cc4b3787e1fcc1cbecac5
Hossein Ranjbar <102860600+HoseinRanjbar@users.noreply.github.com>
2024-10-03 14:39:26 +0200

    Update train_str.sh

--- scripts/train_str.sh
+++ scripts/train_str.sh
@@ -12,6 +12,6 @@
 module load gpu
 module load cuda
 module load python/3.12
-source /home/hranjb/data/ST-TR/env/bin/activate
+source ./env/bin/activate
 
-python code/main.py --config /home/hranjb/data/ST-TR/code/config/st_gcn/islr/train_str.yaml --work-dir /home/hranjb/data/ST-TR/experiments/exp_str_bs64 --phase 'train'
+python ./main.py --config ./config/st_gcn/islr/train_str.yaml --work-dir ./experiments --phase 'train'
